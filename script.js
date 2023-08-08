00a9045d3a38038d1b58111e7db2d55d4ec8f883
Weston Kostura <weston.kostura@gmail.com>
2023-08-07 23:33:19 -0700

    added basic css style and finished results and images

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-var searchForm = document.querySelector('#searchForm')
-searchForm.addEventListener('submit', async function(event) {
-    event.preventDefault();
-    console.log("Submitted form")
-    const searchTerm = searchForm.elements.query.value;
-    const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    addImg(response.data)
-})
+var searchForm = document.querySelector("#searchForm");
+var pictureDiv = document.querySelector(".pictures")
+
+
+searchForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  console.log("Submitted form");
+  const searchTerm = searchForm.elements.query.value;
+  const config = { params: { q: searchTerm } };
+  const response = await axios.get(
+    `https://api.tvmaze.com/search/shows`,
+    config
+  );
+  addImg(response.data);
+  searchForm.elements.query.value = "";
+});
 
 var addImg = (shows) => {
-    for (let result of shows) {
-        if(result.show.image) {
-    const img = document.createElement('img');
-    img.src = result.show.image.medium;
-    document.body.append(img)
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("img");
+      img.src = result.show.image.medium;
+      pictureDiv.append(img);
     }
-    }
-}
+  }
+};
